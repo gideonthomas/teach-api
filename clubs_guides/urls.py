be7a2d4674cf984fef72b53fd88d4dc79f5558a1
Gideon Thomas <gideon@mozillafoundation.org>
2016-08-11 12:02:23 -0400

    Fix #45 - Add urls, views and serializers for /clubsguides

--- clubs_guides/urls.py
+++ clubs_guides/urls.py
@@ -1,3 +1,18 @@
 from django.conf.urls import url
 
-# Create you url mappings here.
+from clubs_guides.views import (
+    ClubsGuidesListView,
+    ClubsGuideView,
+    CategoryListView,
+)
+
+
+urlpatterns = [
+    url("^$", ClubsGuidesListView.as_view(), name="clubsguide-list"),
+    url(r"^(?P<pk>[0-9]+)", ClubsGuideView.as_view(), name="clubsguide"),
+    url(
+        "^categories",
+        CategoryListView.as_view(),
+        name="clubsguide-category-list"
+    ),
+]
